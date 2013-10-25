4a0e6af8aa833b63c162f8d723378e5c20ef5ff6
Wouter Mooij <wouter@wercker.com>
2013-10-25 14:52:17 +0200

    try to show scripts try 5

--- config.rb
+++ config.rb
@@ -56,7 +56,7 @@ set :images_dir, 'images'
 #set :markdown, :fenced_code_blocks => true
 
 set :markdown_engine, :redcarpet
-set :markdown, :fenced_code_blocks => true, :smartypants => true, :escape_html => false
+set :markdown, :fenced_code_blocks => true, :smartypants => true, :layout_engine => :erb
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
